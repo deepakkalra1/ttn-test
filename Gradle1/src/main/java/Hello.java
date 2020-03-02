ba2614e56feaabafb0d864d87ff045b6689d668d
deepakkalra1 <deepak.kalra1@tothenew.com>
2020-03-02 16:49:42 +0530

    gradle exercise

---
+++ Gradle1/src/main/java/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+class testing{
+
+}
+public class Hello extends testing{
+
+    public void test(){
+
+    }
+    public static void main(String[] args) {
+        List<Integer> ls = Arrays.asList(1,2,3,4);
+
+    }
+    }
+
