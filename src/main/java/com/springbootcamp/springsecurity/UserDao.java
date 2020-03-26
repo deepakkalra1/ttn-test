d098d06d17c379f3876ade5bd8f9e8294e6cfaea
deepakkalra1 <deepak.kalra1@tothenew.com>
2020-03-26 18:00:52 +0530

    upload

--- src/main/java/com/springbootcamp/springsecurity/UserDao.java
+++ src/main/java/com/springbootcamp/springsecurity/UserDao.java
@@ -21,6 +21,7 @@ public class UserDao {
         if (username != null) {
             return new AppUser(user.getUsername(), user.getPassword(), Arrays.asList(new GrantAuthorityImpl(user.getRole())));
         } else {
+
             throw new RuntimeException();
         }
 
